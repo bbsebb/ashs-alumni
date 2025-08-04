ff00fb443d7ab8255473befbe73aa2db632a3a1c
bbseb <sebastien.burckhardt@gmail.com>
2025-08-05 01:46:23 +0200

    Add `FormerCard` component to display individual teammate details, including styles, signals, and Angular Material integrations.

--- frontend/src/app/domains/former-teammates/former-card/former-card.ts
+++ frontend/src/app/domains/former-teammates/former-card/former-card.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, Signal} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {CommonModule} from '@angular/common';
+import {FormerTeammate} from '../former-teammates';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {ActivatedRoute} from '@angular/router';
+import {FormerTeammatesStore} from '@app/domains/former-teammates/former-teammates-store';
 
 @Component({
   selector: 'app-former-card',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatDividerModule,
+    MatExpansionModule
+  ],
   templateUrl: './former-card.html',
   styleUrl: './former-card.scss'
 })
 export class FormerCard {
+  paramsRouteSignal = toSignal(inject(ActivatedRoute).params.pipe());
+  formerTeammateSignal: Signal<FormerTeammate | undefined>;
+
+  constructor() {
+    const formerTeammatesStore = inject(FormerTeammatesStore);
+    this.formerTeammateSignal = computed(() => {
+      const formerTeammateId:string |undefined = this.paramsRouteSignal()?.['id'];
+      if(formerTeammateId === undefined) {
+        return undefined
+      }
+      return formerTeammatesStore.getFormerTeammateById(formerTeammateId)();
+    })
+  }
 
 }
